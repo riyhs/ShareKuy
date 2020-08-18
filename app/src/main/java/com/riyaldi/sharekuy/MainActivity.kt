6fe0c4146acac51f4b00431aa0bfb8d1261a6b2e
Riyaldi Hasan <riy.2923@gmail.com>
2020-08-18 22:13:56 +0700

    Initial commit

---
+++ app/src/main/java/com/riyaldi/sharekuy/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.riyaldi.sharekuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+}
+
+//Todo 1. Home (Daftar ShareAn)

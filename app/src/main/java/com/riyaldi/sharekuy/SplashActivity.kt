06acbee1e2b94bc028b00afb89288125bc7c23c6
Riyaldi Hasan <riy.2923@gmail.com>
2020-08-29 20:59:25 +0700

    add splashScreen

---
+++ app/src/main/java/com/riyaldi/sharekuy/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.riyaldi.sharekuy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@InternalCoroutinesApi
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentSplash, SplashFragment(), SplashFragment::class.java.simpleName)
+            .commit()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}

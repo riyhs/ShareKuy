f79e91ab1e9653eab87c432ef0c1420288cd4b1e
Riyaldi Hasan <riy.2923@gmail.com>
2020-09-04 23:02:32 +0700

    fi bug activity_main.xml background

--- app/src/main/java/com/riyaldi/sharekuy/SplashFragment.kt
+++ app/src/main/java/com/riyaldi/sharekuy/SplashFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.Keep
 import androidx.fragment.app.Fragment
-import com.riyaldi.sharekuy.R
 
+@Keep
 class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
